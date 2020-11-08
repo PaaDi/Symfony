3fae5d24509ea3b14de9cfe34a9a09e502a005d4
lexouma <71762044+lexouma@users.noreply.github.com>
2020-11-08 15:21:40 +0100

    Lancement du projet

---
+++ src/Controller/GestionDevisController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GestionDevisController extends AbstractController
+{
+    /**
+     * @Route("/gestiondevis/", name="gestion_devis")
+     */
+    public function index()
+    {
+        return $this->render('gestion_devis/dashboard.html.twig', [
+            'controller_name' => 'GestionDevisController',
+            'headerRechercheOptions' => array("Tout", "Projets", "Chantiers", "Devis", "Clients"),
+            'tachesRapides' => array('Appeler someCompagny, urgent!'
+                                    , 'Erreur dans le devis #454'
+                                    , 'Virer Paul, quel relou'
+                                    , 'MAJ coordonnées Mr. Brabantia')
+        ]);
+    }
+}
