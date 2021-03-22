c5b8377133f89a6b2e980ec68fd200db1c6a4a0e
Maxime Paletou <maxime.paletou@icloud.com>
2021-03-22 17:16:45 +0100

    Add API requests

--- src/Controller/APIgestionDevisController.php
+++ src/Controller/APIgestionDevisController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Login;
+use App\Repository\ChantierRepository;
+use App\Repository\ClientRepository;
+use App\Repository\ContactRepository;
+use App\Repository\ProjetRepository;
 use App\Service\APIgestionDevisService;
 use App\Service\baseService;
 use App\Service\LoginService;
+use PhpParser\Node\Expr\Array_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -72,4 +77,143 @@ class APIgestionDevisController extends AbstractController
         return $response;
     }
 
+    /**
+     * @Route("/API/getallclients", name="API-getallclients")
+     * @return Response
+     */
+    public function getAllClients(ClientRepository $clientRepository)
+    {
+        $request = Request::createFromGlobals();
+
+
+        if ($request->request->get('validrequest') == null)
+        {
+            return new JsonResponse(array(
+                'Erreur' => "Les arguments en POST sont NULL",
+            ));
+        }
+        if ($request->request->get('validrequest') == "OK")
+        {
+            $allClients = $clientRepository->findAll();
+            $arrayAllClients = array();
+
+            foreach ($allClients as $client){
+                array_push($arrayAllClients, array(
+                    "nom" => $client->getNom(),
+                    "adresse" => $client->getAdresse(),
+                    "codepostal" => $client->getCodepostal(),
+                    "ville"=>$client->getVille(),
+                    "estpro"=>$client->getEstprofessionnel(),
+                    "secteur"=>$client->getSecteuractivite(),
+                    "description"=>$client->getDescription(),
+                    "refClient" => $client->getRefClient()
+                ));
+            }
+
+            $response = new JsonResponse(array(
+               "clients" => $arrayAllClients
+            )
+
+            );
+            return $response;
+        }
+        return new JsonResponse(array(
+            'Erreur' => "Il y'a une erreur dans la requête"
+        ));
+
+    }
+
+    /**
+     * @Route("/API/getallcontacts", name="API-getallcontacts")
+     * @return Response
+     */
+    public function getAllContacts(ContactRepository $contactRepository, ClientRepository $clientRepository)
+    {
+
+        $allContacts = $contactRepository->findAll();
+        $arrayAllContacts = array();
+
+        foreach ($allContacts as $contact){
+            array_push($arrayAllContacts, array(
+                "refClient" => $clientRepository->find($contact->getIdclient())->getRefClient(),
+                "nom" => $contact->getNom(),
+                "prenom"=>$contact->getPrenom(),
+                "fonction"=>$contact->getFonction(),
+                "telephone"=>$contact->getTelephone(),
+                "mail"=>$contact->getMail(),
+                "refContact"=>$contact->getRefContact()
+            ));
+        }
+
+        $response = new JsonResponse(
+            $arrayAllContacts
+        );
+        return $response;
+    }
+
+    /**
+     * @Route("/API/getallprojets", name="API-getallprojets")
+     * @return Response
+     */
+    public function getAllProjets(ProjetRepository $projetRepository, ClientRepository $clientRepository)
+    {
+
+
+        $allProjets = $projetRepository->findAll();
+        $arrayAllProjets = array();
+
+        foreach ($allProjets as $projet){
+            $StringDate = $projet->getDatecreation()->format("d-m-Y");
+            array_push($arrayAllProjets, array(
+                "refClient" => $clientRepository->find($projet->getIdclient())->getRefClient(),
+                "refProjet" => $projet->getRefProjet(),
+                "nom" => $projet->getNom(),
+                "dateCreation"=>$StringDate,
+                "notes"=>$projet->getNotes(),
+            ));
+        }
+
+        $response = new JsonResponse(
+            $arrayAllProjets
+        );
+        return $response;
+    }
+
+    /**
+     * @Route("/API/getallchantiers", name="API-getallchantiers")
+     * @return Response
+     */
+    public function getAllChantiers(ChantierRepository $chantierRepository,ProjetRepository $projetRepository)
+    {
+
+
+        $allChantiers = $chantierRepository->findAll();
+        $arrayAllChantiers = array();
+
+        foreach ($allChantiers as $chantier){
+            $StringDateCreation = $chantier->getDatecreation()->format("d-m-Y");
+            $StringDateLancement = $chantier->getDateLancement()->format("d-m-Y");
+            array_push($arrayAllChantiers, array(
+                "idUser" => $chantier->getIduser(),
+                "refProjet" => $projetRepository->find($chantier->getIdprojet())->getRefProjet(),
+                "nom"=>$chantier->getNom(),
+                "notes"=>$chantier->getNotes(),
+                "refChantier"=>$chantier->getRefChantier(),
+                "adresse"=>$chantier->getAdresse(),
+                "ville"=>$chantier->getVille(),
+                "codePostal"=>$chantier->getCodepostal(),
+                "datecreation"=>$StringDateCreation,
+                "datelancement"=>$StringDateLancement
+
+            ));
+        }
+
+        $response = new JsonResponse(
+            $arrayAllChantiers
+        );
+        return $response;
+    }
 }
+
+
+
